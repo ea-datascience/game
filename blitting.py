b99cf65eefa80eb33971b4e0ed12f0385e6f5107
Enrique Acuay <e.acuay@gmail.com>
2022-05-02 12:02:56 +0200

    blitting includes a cache now that accelerates the rendering

--- blitting.py
+++ blitting.py
@@ -25,18 +25,30 @@ img_width = image.size[0]/24
 img_height = image.size[1]/12
 number_of_frames = 6
 
-def run_animation(offset):
+cache = dict()
+
+def run_animation(offset, key):
     
 
-    global i, canvas, image
+    global i, canvas, image, cache
     i = i + 1
     i = i%number_of_frames
     print(i)
     print("does this ever gets executed")
-    dimensions = ( i*img_width + offset[0], 0 + offset[1],\
-                  (i + 1)*img_width + offset[0], img_height + offset[1])
-    cropped = image.crop(dimensions)
-    img= ImageTk.PhotoImage(cropped)
+
+    if not key in cache.keys():
+        cache[key] = dict()
+
+    if i in cache[key].keys():
+        img = cache[key][i]
+    else:
+        dimensions = (      i*img_width + offset[0], 0          + offset[1],\
+                    (i + 1)*img_width + offset[0], img_height + offset[1])
+        cropped = image.crop(dimensions)
+        img= ImageTk.PhotoImage(cropped)
+        cache[key][i] = img
+
+    
 
     canvas.delete('all')
     image_sprite = canvas.create_image(100, 100, image = img)
@@ -48,40 +60,16 @@ def run_animation(offset):
 
 def move(evnt):
     if "Left" == evnt.keysym:
-        run_animation(offset = (0, img_height))
+        run_animation(offset = (0, img_height), key = evnt.keysym)
     elif "Right" == evnt.keysym:
-        run_animation(offset = (0,0))
+        run_animation(offset = (0,0), key = evnt.keysym)
     elif "Down" == evnt.keysym:
-        run_animation(offset = (3 * number_of_frames * img_width,img_height))
+        run_animation(offset = (3 * number_of_frames * img_width,img_height), key = evnt.keysym)
     elif "Up" == evnt.keysym:
-        run_animation(offset = (2 * number_of_frames * img_width,0))
+        run_animation(offset = (2 * number_of_frames * img_width,0), key = evnt.keysym)
 
 
 canvas.bind_all('<Key>', move)
 
 root.mainloop()
 
-
-"""
-#Import the required Libraries
-from tkinter import Tk, Canvas, NW
-from PIL import Image,ImageTk
-
-#Create an instance of tkinter frame
-win = Tk()
-
-#Set the geometry of tkinter frame
-win.geometry("750x270")
-
-#Create a canvas
-canvas= Canvas(win, width= 600, height= 400)
-canvas.pack()
-
-#Load an image in the script
-img= ImageTk.PhotoImage(Image.open("images/spaghetti_atlas.png"))
-
-#Add image to the Canvas Items
-canvas.create_image(10,10,anchor=NW,image=img)
-
-win.mainloop()
-"""
